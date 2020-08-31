6116eb3809f248b900933c5db153537814b9165d
Anthony Chau <achau713@gmail.com>
2020-08-31 15:14:58 -0700

    Return key columns along with summary scores

--- R/compute_summary_score.R
+++ R/compute_summary_score.R
@@ -1,6 +1,7 @@
 #' Compute sum and average scores
 #'
 #' @param df A data frame
+#' @param id_affix A character vector of column(s) that uniquely identify a row
 #' @param var_affix A string forming the variable name pattern. Specifies which columns to use to compute a score
 #' @param score_name The name of the score variable
 #' @param score_type The name of the score type. Currently supports: {average, sum}
@@ -11,6 +12,7 @@
 #' @examples compute_summary_score(stress_questionnaire, var_affix = "physical_stress", score_name = "physical_stress_sum")
 
 compute_summary_score <- function(df,
+                                  id_affix,
                                   var_affix,
                                   score_name,
                                   score_type = "average",
@@ -18,17 +20,23 @@ compute_summary_score <- function(df,
   # Append with informative suffix
   score_name <- paste0(score_name, suffix)
 
-  # Subset data frame by relevant columns to use in score computation
-  df <- subset_column(df, var_affix)
+  # Create regex of id columns and summary columns
+  id_measurement_cols <- paste0(id_affix, "|", var_affix)
+
+  # Subset data frame to include id columns and measurement columns
+  df <- subset_column(df, id_measurement_cols)
+
+  # Subset data frame to include measurement columns
+  df_measurement <- subset_column(df, var_affix)
 
   # compute average score
   if (score_type == "average"){
-    df[[score_name]] <- rowMeans(df, na.rm = TRUE)
+    df[[score_name]] <- rowMeans(df_measurement, na.rm = TRUE)
   }
 
   # compute sum score
   if (score_type == "sum"){
-    df[[score_name]] <- rowSums(df, na.rm = TRUE)
+    df[[score_name]] <- rowSums(df_measurement, na.rm = TRUE)
   }
 
   return(df)
--- tests/testthat/test-compute-summary-score.R
+++ tests/testthat/test-compute-summary-score.R
@@ -6,7 +6,10 @@ test_that("compute_summary_score computes a sum score correctly", {
                             physical_stress_1 = c(3, 5, 1),
                             physical_stress_2 = c(2, 3, 2))
   physical_stress_sum <- c(5, 8, 3)
-  expect_setequal(compute_summary_score(df = stress_data, var_affix = "physical", score_name = "physical_stress_sum", score_type = "sum")[["physical_stress_sum_score"]], physical_stress_sum)
+  id <- c(1:3)
+
+  expect_setequal(compute_summary_score(df = stress_data, id_affix = "subject_id", var_affix = "physical", score_name = "physical_stress_sum", score_type = "sum")[["physical_stress_sum_score"]], physical_stress_sum)
+  expect_setequal(compute_summary_score(df = stress_data, id_affix = "subject_id", var_affix = "physical", score_name = "physical_stress_sum", score_type = "sum")[["subject_id"]], id)
 })
 
 
@@ -16,5 +19,8 @@ test_that("compute_summary_score computes a mean score correctly", {
                             physical_stress_1 = c(3, 5, 1),
                             physical_stress_2 = c(2, 3, 2))
   physical_stress_mean <- c(2.5, 4, 1.5)
-  expect_setequal(compute_summary_score(df = stress_data, var_affix = "physical", score_name = "physical_stress_mean", score_type = "average")[["physical_stress_mean_score"]], physical_stress_mean)
+  id <- c(1:3)
+
+  expect_setequal(compute_summary_score(df = stress_data, id_affix = "subject_id", var_affix = "physical", score_name = "physical_stress_mean", score_type = "average")[["physical_stress_mean_score"]], physical_stress_mean)
+  expect_setequal(compute_summary_score(df = stress_data, id_affix = "subject_id", var_affix = "physical", score_name = "physical_stress_mean", score_type = "average")[["subject_id"]], id)
 })

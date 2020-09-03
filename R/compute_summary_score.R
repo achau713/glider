4af4d8fe44bc490862c39580d14e929b5108c511
Anthony Chau <achau713@gmail.com>
2020-09-03 14:09:04 -0700

    Refactor to remove unnecessary code

--- R/compute_summary_score.R
+++ R/compute_summary_score.R
@@ -1,42 +1,25 @@
 #' Compute sum and average scores
 #'
 #' @param df A data frame
-#' @param id_affix A character vector of column(s) that uniquely identify a row
-#' @param var_affix A string forming the variable name pattern. Specifies which columns to use to compute a score
-#' @param score_name The name of the score variable
+#' @param pattern A regex string specifying which variables to compute summary scores on.
+#' @param score_name The name of the score variable. Specify as string.
 #' @param score_type The name of the score type. Currently supports: {average, sum}
-#' @param suffix Informative suffix to append to variable name.
 #'
-#' @return A data frame with new column of subscale scores. The new column name is concatanation of score_name and "_score"
+#' @return A data frame with new summary score column. The new column name is score_name
 #' @export
-#' @examples compute_summary_score(stress_questionnaire, var_affix = "physical_stress", score_name = "physical_stress_sum")
+#' @examples compute_summary_score(stress_questionnaire, score_name = "physical_stress_average", score_type = "average")
 
 compute_summary_score <- function(df,
-                                  id_affix,
-                                  var_affix,
+                                  pattern,
                                   score_name,
-                                  score_type = "average",
-                                  suffix = "_score") {
-  # Append with informative suffix
-  score_name <- paste0(score_name, suffix)
+                                  score_type = "average") {
 
-  # Create regex of id columns and summary columns
-  id_measurement_cols <- paste0(id_affix, "|", var_affix)
-
-  # Subset data frame to include id columns and measurement columns
-  df <- subset_column(df, id_measurement_cols)
-
-  # Subset data frame to include measurement columns
-  df_measurement <- subset_column(df, var_affix)
-
-  # compute average score
   if (score_type == "average"){
-    df[[score_name]] <- rowMeans(df_measurement, na.rm = TRUE)
+    df[[score_name]] <- rowMeans(subset_column(df, pattern = pattern), na.rm = TRUE)
   }
 
-  # compute sum score
   if (score_type == "sum"){
-    df[[score_name]] <- rowSums(df_measurement, na.rm = TRUE)
+    df[[score_name]] <- rowSums(subset_column(df, pattern = pattern), na.rm = TRUE)
   }
 
   return(df)
--- tests/testthat/test-compute-summary-score.R
+++ tests/testthat/test-compute-summary-score.R
@@ -2,25 +2,31 @@ context("Compute summary score")
 library(glider)
 
 test_that("compute_summary_score computes a sum score correctly", {
-  stress_data <- data.frame(subject_id = c(1:3),
-                            physical_stress_1 = c(3, 5, 1),
-                            physical_stress_2 = c(2, 3, 2))
+  stress <- data.frame(
+    subject_id = c(1:3),
+    physical_stress_1 = c(3, 5, 1),
+    physical_stress_2 = c(2, 3, 2)
+  )
+
   physical_stress_sum <- c(5, 8, 3)
   id <- c(1:3)
 
-  expect_setequal(compute_summary_score(df = stress_data, id_affix = "subject_id", var_affix = "physical", score_name = "physical_stress_sum", score_type = "sum")[["physical_stress_sum_score"]], physical_stress_sum)
-  expect_setequal(compute_summary_score(df = stress_data, id_affix = "subject_id", var_affix = "physical", score_name = "physical_stress_sum", score_type = "sum")[["subject_id"]], id)
+  expect_setequal(compute_summary_score(df = stress, pattern = "^physical_stress", score_name = "physical_stress_sum", score_type = "sum")[["physical_stress_sum"]], physical_stress_sum)
+  expect_setequal(compute_summary_score(df = stress, pattern = "^physical_stress", score_name = "physical_stress_sum", score_type = "sum")[["subject_id"]], id)
 })
 
 
 
 test_that("compute_summary_score computes a mean score correctly", {
-  stress_data <- data.frame(subject_id = c(1:3),
-                            physical_stress_1 = c(3, 5, 1),
-                            physical_stress_2 = c(2, 3, 2))
-  physical_stress_mean <- c(2.5, 4, 1.5)
-  id <- c(1:3)
+  stress <- data.frame(
+    subject_id = c(1:3),
+    physical_stress_1 = c(3, 5, 1),
+    physical_stress_2 = c(2, 3, 2)
+  )
 
-  expect_setequal(compute_summary_score(df = stress_data, id_affix = "subject_id", var_affix = "physical", score_name = "physical_stress_mean", score_type = "average")[["physical_stress_mean_score"]], physical_stress_mean)
-  expect_setequal(compute_summary_score(df = stress_data, id_affix = "subject_id", var_affix = "physical", score_name = "physical_stress_mean", score_type = "average")[["subject_id"]], id)
+  physical_stress_mean <- c(2.5, 4, 1.5)
+  id <- c(1, 2, 3)
+
+  expect_setequal(compute_summary_score(df = stress, pattern = "^physical_stress", score_name = "physical_stress_mean", score_type = "average")[["physical_stress_mean"]], physical_stress_mean)
+  expect_setequal(compute_summary_score(df = stress, pattern = "^physical_stress", score_name = "physical_stress_mean", score_type = "average")[["subject_id"]], id)
 })
